e3aebe3b8f9b8fd1a7beb58d466b2b2d33423778
subash <subashrijal5@gmail.com>
2023-06-23 12:38:33 +0900

    asdfg

--- resources/js/Pages/Post/Fields.jsx
+++ resources/js/Pages/Post/Fields.jsx
@@ -9,7 +9,26 @@ import BasicCard from "@/Components/Utility/Cards/BasicCard";
 import MultiSelect from "@/Components/Utility/Select/MultiSelect";
 import Autocomplete from "@/Components/Autocomplete";
 import { useQuery } from "@tanstack/react-query";
-import { fetchLanguages, fetchPostCategories, fetchTags } from "@/Hooks/api";
+import { fetchLanguages, fetchPostCategories } from "@/Hooks/api";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+
+const help = {
+    name: "help",
+    keyCommand: "help",
+    buttonProps: { "aria-label": "Insert help" },
+    icon: (
+        <svg viewBox="0 0 16 16" width="12px" height="12px">
+            <path
+                d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8Zm.9 13H7v-1.8h1.9V13Zm-.1-3.6v.5H7.1v-.6c.2-2.1 2-1.9 1.9-3.2.1-.7-.3-1.1-1-1.1-.8 0-1.2.7-1.2 1.6H5c0-1.7 1.2-3 2.9-3 2.3 0 3 1.4 3 2.3.1 2.3-1.9 2-2.1 3.5Z"
+                fill="currentColor"
+            />
+        </svg>
+    ),
+    execute: (state, api) => {
+        window.open("https://www.google.com", "_blank");
+    },
+};
 
 const PostField = ({
     auth,
@@ -126,16 +145,16 @@ const PostField = ({
                                             forInput="description"
                                             value="Description"
                                         />
+                                        <CKEditor
+                                            editor={ClassicEditor}
+                                            data={data.description}
 
-                                        <TextInput
-                                            id="description"
-                                            name="description"
-                                            type="textarea"
-                                            value={data.description}
-                                            className="block w-full mt-1"
-                                            autoComplete="description"
-                                            isFocused={true}
-                                            handleChange={onHandleChange}
+                                            onBlur={(event, editor) => {
+                                                onHandleChange(
+                                                    "description",
+                                                    editor.getData()
+                                                );
+                                            }}
                                         />
 
                                         <InputError
